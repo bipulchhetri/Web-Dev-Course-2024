c2f34cc581ead439978883d8d6bd0ca6e56ee748
Bipul chhetri <81376867+bipulchhetri@users.noreply.github.com>
2024-02-21 14:10:51 +0530

    HOTEL UI Project

---
+++ Hotel UI/script.js
@@ -0,0 +1,10 @@
+var menulist = document.getElementById("menulist");
+menulist.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (menulist.style.maxHeight == "0px") {
+    menulist.style.maxHeight = "100vh";
+  } else {
+    menulist.style.maxHeight = "0px";
+  }
+}
